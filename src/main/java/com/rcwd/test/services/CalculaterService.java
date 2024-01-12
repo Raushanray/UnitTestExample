83f14aff7d466cc0e7d8a9fda50d983077e125f7
Raushanray <raushankuray1998@gmail.com>
2024-01-12 19:30:48 +0530

    Creata a maven project and here create calculater class and here create some method and also add test case of all the methods using JUnit-4.

---
+++ src/main/java/com/rcwd/test/services/CalculaterService.java
@@ -0,0 +1,20 @@
+package com.rcwd.test.services;
+
+public class CalculaterService {
+    public static int addTwoNumbers(int a, int b){
+        return a+b;
+    }
+    public static int productTwoNumbers(int a, int b){
+        return a*b;
+    }
+    public static int devideTwoNumbers(int a, int b){
+        return a/b;
+    }
+    public static int sunAnyNumbers(int ...numbers){
+        int s =0;
+        for (int n:numbers){
+            s+=n;
+        }
+        return s;
+    }
+}
---
+++ src/test/java/com/rcwd/test/UnitTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcwd.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
